6c6effa1c57127a0fad8edc609e49000a75a8f57
Natallia-Lu <kali.laska@mail.ru>
2023-02-15 16:05:58 +0100

    feat: add morse

--- src/index.js
+++ src/index.js
@@ -39,6 +39,33 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let size = 10
+    let i = 0
+    let count = expr.length / size
+    let str = ''
+     let cod = ''
+
+    for (i; i < count; i++) {   
+        let decod = ''
+        str = expr.substr(i*size, size)
+        for (let j = 0; j < 10; j = j + 2) {
+            if (str.substr(j, 2) === '10') {
+                decod = decod + '.'
+            } else if (str.substr(j, 2) === '11') {
+                decod = decod + '-'
+            } else if (str.substr(j, 2) === '*') {
+                decod = decod + ' '
+            } else if (str.substr(j, 2) === '00') {
+                decod = decod
+            }
+        }
+        if (MORSE_TABLE[decod] === undefined) {
+            cod = cod + ' '
+        }else {
+            cod = cod + MORSE_TABLE[decod]
+        }
+    }
+    return cod
 }
 
 module.exports = {
